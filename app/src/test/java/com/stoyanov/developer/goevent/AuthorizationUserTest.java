2dd08ee780828bbecb8e1800c89a087fec4091ba
Vitaliy <developer.stoyanov@gmail.com>
2016-09-25 14:04:04 +0300

    Initial commit

---
+++ app/src/test/java/com/stoyanov/developer/goevent/AuthorizationUserTest.java
@@ -0,0 +1,54 @@
+package com.stoyanov.developer.goevent;
+
+import android.os.Build;
+
+import com.google.gson.JsonObject;
+import com.koushikdutta.ion.Ion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
+
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = Build.VERSION_CODES.M)
+public class AuthorizationUserTest implements MappingAPI {
+
+    @Test
+    public void loginUserTest() throws Exception {
+        JsonObject json = new JsonObject();
+        json.addProperty("username", "melka");
+        json.addProperty("password", "1234");
+        System.out.println("loginUserTest: Body: " + Ion.with(RuntimeEnvironment.application)
+                .load(LOGIN_ENDPOINT)
+                .setJsonObjectBody(json)
+                .asString()
+                .get());
+    }
+
+    @Test
+    public void userEventsTest() throws Exception {
+        String response = Ion.with(RuntimeEnvironment.application)
+                .load(USER_EVENTS_ENDPOINT)
+                .asString()
+                .get();
+        System.out.println("userEventsTest: Body: " + response);
+        assertThat(response, allOf(containsString("_id"), containsString("eventCreator")));
+    }
+
+    @Test
+    public void logoutUserTest() throws Exception {
+        String response = Ion.with(RuntimeEnvironment.application)
+                .load(LOGOUT_ENDPOINT)
+                .setStringBody("")
+                .asString()
+                .get();
+        System.out.println("logoutUserTest: Body: " + response);
+        assertThat(response, allOf(containsString("Success"), containsString("logout")));
+    }
+}
---
+++ app/src/test/java/com/stoyanov/developer/goevent/MappingAPI.java
@@ -0,0 +1,10 @@
+package com.stoyanov.developer.goevent;
+
+public interface MappingAPI {
+
+    String BASE_URL = "http://localhost:8000/v1.0/user/";
+    String USER_EVENTS_ENDPOINT = BASE_URL + "user_events";
+    String LOGIN_ENDPOINT = BASE_URL + "login";
+    String LOGOUT_ENDPOINT = BASE_URL + "logout";
+
+}
