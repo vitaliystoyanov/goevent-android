60aec4822f08759a985fc79a53a729ed5afaab24
Vitaliy <developer.stoyanov@gmail.com>
2016-10-24 22:36:21 +0300

    Styling login activity

--- app/src/main/java/com/stoyanov/developer/goevent/ui/activity/LoginActivity.java
+++ app/src/main/java/com/stoyanov/developer/goevent/ui/activity/LoginActivity.java
@@ -3,6 +3,7 @@ package com.stoyanov.developer.goevent.ui.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.stoyanov.developer.goevent.R;
 
@@ -12,5 +13,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        findViewById(R.id.login_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
